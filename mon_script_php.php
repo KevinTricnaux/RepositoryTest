327a681003b04f2d8ae76742f789007bc3b8ea92
KevinTricnaux <tricnauxkevin@gmail.com>
2017-04-22 15:26:34 +0200

    Update mon_script_php.php

--- mon_script_php.php
+++ mon_script_php.php
@@ -2,6 +2,7 @@
 
 echo '<h1>Bonjour à tous bande de nazes!</h1>';
 echo '<p>Ajout de 14h19</p>';
-echo '<p>Ajout de 15h16</p>';
+echo '<p>Ajout de 15h16 local</p>';
+echo '<p>Ajout de 15h26 en ligne</p>';
 
 ?>
